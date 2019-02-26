b1c7250d72a0dafa052251e87fb595c6bc98e7f8
Juan Galdames <juan.galdames.toledo@gmail.com>
2019-02-25 23:05:51 -0300

    adding feature add and list tickets

--- controllers/ticket.controller.js
+++ controllers/ticket.controller.js
@@ -2,7 +2,15 @@ const Ticket = require('../models/ticket.models');
 
 
 module.exports.listTickets = async () => {
-    return "lists of tickets";
+    return new Promise((resolve, reject) => {
+        Ticket.find({}, (err, tickets) => {
+            if(err) {
+                reject(err);
+            } else {
+                resolve(tickets);
+            }
+        });
+    }) 
 }
 
 module.exports.getTicket = async (id) => {
@@ -10,8 +18,28 @@ module.exports.getTicket = async (id) => {
 }
 
 module.exports.addTicket = async (ticket) => {
-    console.log(ticket);
-    return "adding ticket : ";
+
+    const newTicket = new Ticket({
+        ticketNum: ticket.body.ticketNum,
+        title: ticket.body.title,
+        dateStart: ticket.body.dateStart,
+        dateEnd: new Date(ticket.body.dateEnd),
+        description: ticket.body.description,
+        assigned: ticket.body.assigned,
+        priority: ticket.body.priority,
+        type: ticket.body.type,
+        state: ticket.body.state,
+        resolution: ticket.body.resolution,
+        progress: ticket.body.progress
+    });
+
+    newTicket.save((err, data) => {
+        if(err) {
+            throw err;
+        }
+    });
+
+    return ticket.body;
 }
 
 module.exports.updateTicket = async (ticket) => {
